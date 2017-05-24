c07724af6195b8aae3fd433396edc860ea5ff0d9
Aliaksei Kasiyanik <aleksey.kasianik@gmail.com>
2017-05-24 15:44:46 +0300

    diploma update

---
+++ java/trip/src/main/java/com/akasiyanik/trip/cplex/SpringRunner.java
@@ -0,0 +1,8 @@
+package com.akasiyanik.trip.cplex;
+
+/**
+ * @author akasiyanik
+ *         5/22/17
+ */
+public class SpringRunner {
+}
---
+++ java/trip/src/main/java/com/akasiyanik/trip/domain/network/ArcFactory.java
@@ -0,0 +1,8 @@
+package com.akasiyanik.trip.domain.network;
+
+/**
+ * @author akasiyanik
+ *         5/22/17
+ */
+public class ArcFactory {
+}
